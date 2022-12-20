c36e2f37155efc71d4eba1360b811f0d16c9c0b8
Beef Webster <c_goldsmith@yahoo.com>
2022-12-19 18:27:26 -0800

    finished webpack functionality

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,7 +19,22 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin()
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "It's a browser-based text editor!",
+        start_url: "/",
+        publicPath: "/"
+      })
     ],
 
     module: {
